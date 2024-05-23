91e94e51aeebade6ba9eedaa71a2acd87c948ee1
hryktkuch <hightraffic@gmail.com>
2024-05-23 16:19:50 +0900

    ひとまず動く

--- app.js
+++ app.js
@@ -31,14 +31,70 @@ $("#upload").on("change", function(e) {
 $("#save").on("click", function() {
     var img = document.getElementById("preview");
     var b64=ImageToBase64(img, "image/jpeg");
-    localStorage.setItem("samplekey", b64);
+    var date = new Date();
+    var y=date.getFullYear();
+    var m=date.getMonth()+1;
+    var d=date.getDate();
+    //console.log(d.getDate());
+    var datalist={y,m,d,b64};
+    localStorage.setItem(localStorage.length, JSON.stringify(datalist));
     console.log("saved");
+    console.log(datalist[0]);
 });
 
-$("#load").on("click", function() {
-    var b64=localStorage.getItem("samplekey");
+var correct;
+
+$("#start").on("click", function() {
+    var r=Math.floor(Math.random()*localStorage.length);
+    console.log(r);
+    let datalist=JSON.parse(localStorage.getItem(r));
+    //console.log(datalist[0]);
+    console.log(datalist.y);
+    console.log(datalist.m);
+    console.log(datalist.d);
+    var date=new Date(datalist.y,datalist.m,datalist.d);
+    console.log(date);
+    var b64=datalist.b64;
     Base64ToImage(b64, function(img) {
-        document.getElementById("preview2").src = img.src;
+        document.getElementById("quiz").src = img.src;
     });
+    //$("#date").html(datalist.y+" "+datalist.m+" "+datalist.d);
+    correct=Math.floor(Math.random()*3);
+    if (correct==0) {
+        $("#button0").text(datalist.y+"/"+datalist.m+"/"+(datalist.d));
+        $("#button1").text(datalist.y+"/"+datalist.m+"/"+(datalist.d+1));
+        $("#button2").text(datalist.y+"/"+datalist.m+"/"+(datalist.d+2));
+    } else if (correct==1) {
+        $("#button0").text(datalist.y+"/"+datalist.m+"/"+(datalist.d-1));
+        $("#button1").text(datalist.y+"/"+datalist.m+"/"+(datalist.d));
+        $("#button2").text(datalist.y+"/"+datalist.m+"/"+(datalist.d+1));
+    } else {
+        $("#button0").text(datalist.y+"/"+datalist.m+"/"+(datalist.d-2));
+        $("#button1").text(datalist.y+"/"+datalist.m+"/"+(datalist.d-1));
+        $("#button2").text(datalist.y+"/"+datalist.m+"/"+(datalist.d));
+    }
     console.log("loaded");
+    $("#seigo").text("正解／不正解はここに表示");
+});
+
+$("#button0").on("click", function() {
+    if (correct==0) {
+        $("#seigo").text("正解");
+    } else {
+        $("#seigo").text("不正解");
+    }
+});
+$("#button1").on("click", function() {
+    if (correct==1) {
+        $("#seigo").text("正解");
+    } else {
+        $("#seigo").text("不正解");
+    }
+});
+$("#button2").on("click", function() {
+    if (correct==2) {
+        $("#seigo").text("正解");
+    } else {
+        $("#seigo").text("不正解");
+    }
 });
